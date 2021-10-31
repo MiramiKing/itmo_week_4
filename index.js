56cd0d005b4115a0382964f0bd88ae4a74d2a4de
Mikhail Ivanov <mishaivanov99@gmail.com>
2021-10-31 18:56:50 +0300

    port

--- index.js
+++ index.js
@@ -6,21 +6,37 @@ let CORS = {
     'Access-Control-Allow-Headers':
         'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
 };
-
+let qs = require('querystring');
 http.Server((req, res) => {
-    if (req.url === '/result4/' && (req.method === 'GET' || req.method === 'POST')) {
+
+
+    if (req.method === 'POST') {
+        var body = '';
+
+        req.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                req.connection.destroy();
+        });
+
+        req.on('end', function () {
+            //var post = qs.parse(body);
+            // use post['blah'], etc.
+        });
         let result = {
             'message': 'itmo337560',
             'x-result': req?.headers?.['x-test'],
-            'x-body': req?.body
+            'x-body': body
         };
 
         res.writeHead(200, {...CORS, 'Content-Type': 'application/json'});
         res.end(JSON.stringify(result));
 
 
-    }
-    else {
+    } else {
         res.writeHead(200, {...CORS, 'Content-Type': 'text/plain'});
         res.end('OK');
     }
