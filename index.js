30ab15acbae7ab91f02da3973a997fa31f01a327
Mikhail Ivanov <mishaivanov99@gmail.com>
2021-10-31 18:10:33 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+let CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+    'Access-Control-Allow-Headers':
+        'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
+};
+
+http.Server((req, res) => {
+    if (req.url === '/result4/' && (req.method === 'GET' || req.method === 'POST')) {
+        let result = {
+            'message': 'itmo337560',
+            'x-result': req?.headers?.['x-test'],
+            'x-body': req?.body
+        };
+
+        res.writeHead(200,{...CORS, 'Content-Type': 'application/json'});
+        res.end(JSON.stringify(result));
+
+
+    }
+}).listen(8000)
