b810743f9689848e9617fbfd8ae5ab102a4bbdea
Mikhail Ivanov <mishaivanov99@gmail.com>
2021-10-31 19:07:04 +0300

    fix post

--- index.js
+++ index.js
@@ -6,34 +6,28 @@ let CORS = {
     'Access-Control-Allow-Headers':
         'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
 };
-let qs = require('querystring');
+
 http.Server((req, res) => {
 
 
     if (req.method === 'POST') {
         var body = '';
 
-        req.on('data', function (data) {
-            body += data;
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
 
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                req.connection.destroy();
+            let result = {
+                'message': 'itmo337560',
+                'x-result': req?.headers?.['x-test'],
+                'x-body': body
+            };
+
+            res.writeHead(200, {...CORS, 'Content-Type': 'application/json'});
+            res.end(JSON.stringify(result));
         });
 
-        req.on('end', function () {
-            //var post = qs.parse(body);
-            // use post['blah'], etc.
-        });
-        let result = {
-            'message': 'itmo337560',
-            'x-result': req?.headers?.['x-test'],
-            'x-body': body
-        };
-
-        res.writeHead(200, {...CORS, 'Content-Type': 'application/json'});
-        res.end(JSON.stringify(result));
 
 
     } else {
